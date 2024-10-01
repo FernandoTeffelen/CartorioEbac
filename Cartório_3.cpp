24a449bb5b7dac947c5a6c198b7528e92cd892d3
Fernando T. <wayszer23@gmail.com>
2024-10-01 20:11:45 -0300

    Opção de saida
    Incluso no sistema a opção de saida para o usuário

--- Cartório_3.cpp
+++ Cartório_3.cpp
@@ -126,6 +126,7 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n");
+        printf("\t4 - Sair do sistema\n");
         printf("Opção: ");
         
         scanf("%d", &opcao); // armazenando a escolha do usuário
@@ -144,6 +145,12 @@ int main()
             case 3:
                 deletar();
                 break;
+            
+            case 4:
+                printf("Obrigado por utilziar o sistema");
+                return 0;
+                break;
+                
                 
             default:
                 printf("Essa opção não está disponível!\n");
